88d0725e84298e58c4fb2bbf4eaaf988d257bdf7
Ken Graf <kengraf57@gmail.com>
2024-12-27 21:13:23 -0500

    Update cooky.js

--- cooky.js
+++ cooky.js
@@ -1,18 +1,33 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const app = express();
-   app.use(cookieParser());
+const PORT = 3000;
 
+// With middleware
+app.use('/', function (req, res, next) {
+    res.cookie('title', 'GeeksforGeeks');
+    res.send("Cookie Set");
+    next();
+})
 
-app.get('/', (req, res) => {
-  res.cookie('myCookie', 'value', { samesite: 'lax', secure: false, httpOnly: false, path: '/' });
-   console.log(res);
-   res.redirect('http://localhost:3000/another-page')
+app.get('/', function (req, res) {
+    console.log('Cookie SET');
 });
 
-app.get('/another-page', (req, res) => {
+
+app.get('/a', (req, res) => {
+  res.cookie('myCookie', 'value', { samesite: 'lax', secure: false, httpOnly: false, path: '/' });
+   console.log(res);
+   res.send("ok");
+   res.redirect('/b')
+});
+
+app.get('/b', (req, res) => {
   console.log(req.cookies); // Should log the cookie
   res.send('Redirected page');
 });
 
-app.listen(3000);
+app.listen(PORT, function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
+
