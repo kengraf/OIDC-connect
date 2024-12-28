ed4f56782c6ce5168fc767c1edbbbb2a17b5725d
Ken Graf <kengraf57@gmail.com>
2024-12-27 20:49:16 -0500

    Update cooky.js

--- cooky.js
+++ cooky.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.cookie('myCookie', 'value', { secure: false, httpOnly: false, path: '/' });
-   console.log(res.cookie);
+   console.log(res.cookies);
    res.redirect('http://localhost:3000/another-page')
 });
 
